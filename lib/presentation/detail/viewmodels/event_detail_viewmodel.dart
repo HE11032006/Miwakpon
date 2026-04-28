a02d958e0555e8970e912d9251678b2b3a2afd43
OSSENI Rosmiyath <rosmiros52@gmail.com>
2026-04-28 23:31:50 +0100

    Création de EventDetailViewModel pour charger un événement par ID

--- lib/presentation/detail/viewmodels/event_detail_viewmodel.dart
+++ lib/presentation/detail/viewmodels/event_detail_viewmodel.dart
@@ -1,59 +1,33 @@
 import 'package:flutter/material.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../data/models/event_model.dart';
-import '../../../core/network/supabase_config.dart';
-
-// TODO: Implémentation par Membre 4/5
-// ViewModel pour les détails d'un événement.
-//
-// Responsabilités :
-// - Charger les détails d'un événement par ID
-// - Gérer la participation (rejoindre / quitter)
-// - Afficher la liste des participants
-//
-// Note : Utilisez EventService pour le temps réel quand vous implémentez.
-// import '../../../data/services/event_service.dart';
 
 class EventDetailViewModel extends ChangeNotifier {
-
+  final _supabase = Supabase.instance.client;
   EventModel? _event;
-  bool _isLoading = true;
-  String? _errorMessage;
+  bool _isLoading = false;
 
   EventModel? get event => _event;
   bool get isLoading => _isLoading;
-  String? get errorMessage => _errorMessage;
 
-  /// Charge les détails d'un événement.
-  Future<void> loadEvent(String eventId) async {
-    // TODO: Implémentation par Membre 4/5
+  Future<void> loadEvent(String id) async {
     _isLoading = true;
-    _errorMessage = null;
     notifyListeners();
 
     try {
-      final response = await SupabaseConfig.client
+      final data = await _supabase
           .from('events')
           .select()
-          .eq('id', eventId)
+          .eq('id', id)
           .single();
 
-      _event = EventModel.fromMap(response);
+      _event = EventModel.fromMap(data);
     } catch (e) {
-      _errorMessage = e.toString();
+      debugPrint("Erreur lors du chargement du détail: $e");
+      _event = null;
     } finally {
       _isLoading = false;
       notifyListeners();
     }
-  }
-
-  /// Rejoint un événement.
-  Future<void> joinEvent(String eventId) async {
-    // TODO: Implémentation par Membre 5
-  }
-
-  /// Quitte un événement.
-  Future<void> leaveEvent(String eventId) async {
-    // TODO: Implémentation par Membre 5
   }
 }
